9af6e2fad4e0f2bf7367db90f0fa4bdada4fdf08
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-30 15:07:48 -0700

    index/layout

---
+++ public/scripts/jquery_helpers.js
@@ -0,0 +1,34 @@
+$('document').ready(
+  function(){
+
+    $('.unhide.bowls0').on('click', function() {
+      console.log('clicked')
+      $('.bowls0').toggleClass('hide')
+    });
+    $('.unhide.bowls1').on('click', function () {
+      $('.bowls1').toggleClass('hide')
+    })
+    $('.unhide.bowls2').on('click', function() {
+      $('.bowls2').toggleClass('hide')
+    })
+    $('.unhide.salads0').on('click', function() {
+      $('.salads0').toggleClass('hide')
+    })
+    $('.unhide.salads1').on('click', function() {
+      $('.salads1').toggleClass('hide')
+    })
+    $('.unhide.salads2').on('click', function() {
+      $('.salads2').toggleClass('hide')
+    })
+    $('.unhide.drinks1').on('click', function() {
+      $('.drinks1').toggleClass('hide')
+    })
+    $('.unhide.drinks2').on('click', function() {
+      $('.drinks2').toggleClass('hide')
+    })
+    $('.unhide.drinks0').on('click', function() {
+      $('.drinks0').toggleClass('hide')
+    })
+  }
+  
+);
