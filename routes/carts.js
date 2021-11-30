f0e2d330c94b3c4c4e33ecda814adbb8d8bf4317
Veronika Fatikhova <fidelio@list.ru>
2021-11-30 00:53:21 -0800

    routes/carts TODO

---
+++ routes/carts.js
@@ -0,0 +1,84 @@
+/*
+ * All routes for Carts are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT * FROM orders`; // todo: where type='order'
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const orders = data.rows;
+        res.json({ orders });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    let query = `SELECT * FROM orders WHERE id = $1`; // todo same
+    let options = [req.params.id];
+
+    console.log(query);
+    db.query(query, options)
+      .then(data => {
+        const orders = data.rows;
+        res.json({ orders });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => { // (user_id, type) values($1, 'cart')
+    let query = `INSERT INTO orders(user_id)
+    VALUES($1)
+    RETURNING *;`;
+    let options = [req.body.id];
+
+    console.log(query);
+    db.query(query, options)
+      .then(data => {
+        const orders = data.rows;
+        res.json({ orders });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.put("/:id", (req, res) => { //update
+    //if completed then
+    let query = `UPDATE orders SET special_instructions
+    VALUES($1) WHERE order_id = $2`;
+
+
+    let options = [req.body.special_instructions, req.params.id];
+
+    console.log(query);
+    db.query(query, options)
+      .then(data => {
+        const orders = data.rows;
+        res.json({ orders });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
