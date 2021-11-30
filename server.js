28ef4ff87b72577a06b19ec8f7bf06b258b194d1
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-30 15:20:54 -0700

    server.js

--- server.js
+++ server.js
@@ -42,12 +42,11 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const ordersRoutes = require("./routes/orders");
 const cartsRoutes = require("./routes/carts");
-<<<<<<< HEAD
-const {getMenuItems} = require("./db/db_pg");
-=======
-// const {getMenuItems} = require("./db/db_pg")
 
->>>>>>> cf73f0ec7907e4df53f4606ef419adeb20597371
+const {getMenuItems} = require("./db/db_pg");
+
+
+ 
 
 // Mount all resource routes
 app.use(usersRoutes(db));
