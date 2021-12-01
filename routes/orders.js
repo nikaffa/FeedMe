9b34ed785cbd2485580a70e297ccc3b924bc5bab
Veronika Fatikhova <fidelio@list.ru>
2021-12-01 01:00:01 -0800

    modify db schema and db seeds, add cookie-parser, modify get / in routes/orders, modify adminOrders.ejs STILL NEEDS TO BE CHANGED

--- routes/orders.js
+++ routes/orders.js
@@ -10,12 +10,28 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM orders WHERE type='order'`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const orders = data.rows;
-        const templateVars = { orders };
+    const promises = [];
+    let query = `
+    SELECT orders.id as incoming_orderId,  items.name, quantity, price, special_instructions FROM orders
+    JOIN order_items ON orders.id = order_id
+    JOIN items ON items.id = item_id
+    WHERE orders.type='order' and accepted_at is null and completed = FALSE
+    GROUP BY incoming_orderId, quantity, items.name, items.price ORDER BY incoming_orderId`;
+    promises.push(db.query(query));
+    query = `
+    SELECT orders.id as current_orderId,  items.name, quantity, price, special_instructions FROM orders
+    JOIN order_items ON orders.id = order_id
+    JOIN items ON items.id = item_id
+    WHERE orders.type='order' and accepted_at is not null and completed = FALSE
+    GROUP BY current_orderId, quantity, items.name, items.price ORDER BY current_orderId`;
+    promises.push(db.query(query));
+
+    Promise.all(promises)
+      .then(all => {
+        const currentOrders = all[0].rows;
+        console.log(currentOrders);
+        const incomingOrders = all[1].rows;
+        const templateVars = { incomingOrders, currentOrders };
         res.render("adminOrders", templateVars);
       })
       .catch(err => {
@@ -63,7 +79,7 @@ module.exports = (db) => {
       });
   });
   router.post("/:id/complete", (req, res) => { //if accepted
-    let query = `UPDATE orders SET accepted_at = CURRENT_TIMESTAMP
+    let query = `UPDATE orders SET completed = TRUE
     WHERE id = $1 AND type = 'order'`;
     let options = [req.params.id]; //order.id??
     console.log(query);
